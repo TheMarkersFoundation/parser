e53c38038c70e7ee00e590dbf28c102bffbc91c0
mirvoxtm <158512424+devsRDC@users.noreply.github.com>
2025-05-12 02:04:43 -0300

    Updating Markers Version

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,10 @@ module Main where
 import Markers
 import System.Environment (getArgs)
 import System.FilePath (replaceExtension)
+import System.Directory (listDirectory)
+import System.FilePath (takeExtension, (</>))
+import qualified Codec.Archive.Zip as Zip
+import qualified Data.ByteString.Lazy as L
 
 printHelp :: IO ()
 printHelp = putStrLn "Usage: mks [-html <file-name> | -markdown <file-name> | -md <file-name> | -abnt <file-name> | -help | -h]"
@@ -25,7 +29,7 @@ main = do
             writeFile (replaceExtension filePath ".md") (convertToMarkdown content)
         ["-abnt", filePath] -> do
             content <- readFile filePath
-            writeFile (replaceExtension filePath ".abnt") (convertToAbnt content)
+            writeFile (replaceExtension filePath ".html") (convertToAbnt content)
         ["-help"] -> printHelp
         ["-h"] -> printHelp
         _ -> printHelp
